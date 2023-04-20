f80c19fa0c0c450795b21b7d4c0386ed7a4db7c5
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2023-04-21 06:23:21 +0700

    Fix: убрал version key из запроса

--- models/card.js
+++ models/card.js
@@ -4,7 +4,8 @@ const cardSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],     maxlength: [30, 'Максимальная длина поля "name - 30'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name - 30'],
   },
   link: {
     type: String,
@@ -24,6 +25,7 @@ const cardSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  { versionKey: false },
 });
 
 module.exports = mongoose.model('card', cardSchema);
