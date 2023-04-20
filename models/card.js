33023054231980c618c45840fd84cfae7b80bafd
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2023-04-21 06:19:25 +0700

    Fix: исправил баг

--- models/card.js
+++ models/card.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const cardSchema = new mongoose.Schema({
   name: {
-    type: String,     required: [true, 'Поле "name" должно быть заполнено'],
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля "name" - 2'],     maxlength: [30, 'Максимальная длина поля "name - 30'],
   },
   link: {
