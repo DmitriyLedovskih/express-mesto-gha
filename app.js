d89b4663d1fb7365bd235ad51df5b04a9e3b1821
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-05-14 21:37:51 +0700

    Feat: добавил мидлвейр cors

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const cookieParser = require('cookie-parser');
 const error = require('./middlewares/error');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
 app.use(express.json());
-
 app.use(cookieParser());
 
 app.use(requestLogger);
+
+app.use(cors);
 
 app.use(router);
 
