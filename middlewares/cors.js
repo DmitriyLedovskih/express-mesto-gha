2f84fb2a553ee1fd49325922d47d568bd335395a
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-05-14 21:38:14 +0700

    Feat: добавил мидлвейр cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Credentials', true);
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
